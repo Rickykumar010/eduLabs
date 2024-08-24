0424a99f796635f962fbcc4b303902b7ab986a16
Ricky Kumar <anitasah926@gmail.com>
2024-08-24 10:45:18 +0530

    education_labs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// app.js
 require('dotenv').config();
 const express = require('express');
 const { mongoConnect } = require('./configs/db');
@@ -7,13 +6,13 @@ const { taskRouter } = require('./router/taskRouter');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json())
 app.use( userRouter);
 app.use( taskRouter);
 
 app.listen(port, async () => {
     try {
-        await mongoConnect(process.env.MONGODB_URL);
+        await mongoConnect(process.env.MONGODB_URL)
         console.log(`Listening on port ${port}`);
     } catch (err) {
         console.error(err);
